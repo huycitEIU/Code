02344048dd5d176ca16530aa5ce79d5a099ffa4a
Trần Gia Huy <huy.trangia.cit23@eiu.edu.vn>
2025-04-27 23:09:42 +0700

    .

---
+++ EIU/src/LightCount.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LightCount
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        long lights = scanner.nextLong();
+        long left  = scanner.nextLong();
+        long right = scanner.nextLong();
+        long i = 1;
+        int lightCounts = 0;
+        while (true)
+        {
+            long t = i*i;
+            if (t > lights)
+            {
+                break;
+            }
+            if (left <= t && t <= right)
+            {
+               lightCounts++;
+            }
+            i++;
+        }
+        System.out.println(lightCounts);
+    }
+}
